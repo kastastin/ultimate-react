c33ad7ce43262e43bbcda5ecd2c04212710a2f72
kastastin <kostya242421@gmail.com>
2023-12-21 02:03:31 +0200

    worldwise nested, dynamic routes + url parameters, query string, useNavigate, <Navigate />

---
+++ worldwise/src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+import styles from "./CountryList.module.css";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, curr) => {
+    if (!acc.map((el) => el.country).includes(curr.country))
+      return [...acc, { country: curr.country, emoji: curr.emoji }];
+    else return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
